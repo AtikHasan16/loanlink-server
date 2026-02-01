e329a0354f1eaff5d0006097558fc3f5004e938e
Hasan MD Atik <akhasannaeem@gmail.com>
2026-02-01 21:13:34 +0600

    fixed some error

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const paymentInfoCollection = database.collection("payment_info");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     //******** Middleware to verify roles ******
     // verify admin
@@ -411,7 +411,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
